95c6273eea965fc16143828720c1302e97f4f08d
Usama <usama.asghar@hotmail.co.uk>
2024-03-12 11:02:37 +0000

    POST is successfull

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
 from postcode_class import *
 from payload_class import *
-import json
-import requests
+
 
 # postcode = "se181bb"
 # parser = GETPostcodeParser(postcode)
 # parser.write_json_file("se181bb_json.json")
-#
+
 # print(parser.postcode)
 # print(parser.status)
 # print(parser.postcode)
@@ -14,14 +13,9 @@ import requests
 # print(parser.longitude)
 # print(parser.latitude)
 
-# payload = {"postcodes": ["se181bb", "se288at"]}
-#
-# res = requests.post("https://api.postcodes.io/postcodes", data=payload)
-# api_dict = res.json()
-# with open("post_json_file.json", "w") as json_file:
-#     json.dump(api_dict, json_file)
 
 payload = {"postcodes": ["se181bb", "se288at"]}
 postcode_parser = POSTPostcodeParser(payload)
 postcode_parser.save_to_json_file("post_json_file.json")
-
+postcode_parser.postcode_details("se181bb")
+postcode_parser.postcode_details("se288at")
--- payload_class.py
+++ payload_class.py
@@ -16,3 +16,12 @@ class POSTPostcodeParser:
     def save_to_json_file(self, filename):
         with open(filename, "w") as json_file:
             json.dump(self.postcode_info, json_file)
+
+    def postcode_details(self, postcode):
+        for item in self.postcode_info["result"]:
+            if item["query"] == postcode:
+                print("Postcode:", item["result"]["postcode"])
+                print("Region:", item["result"]["region"])
+                print("Longitude:", item["result"]["longitude"])
+                print("Latitude:", item["result"]["latitude"])
+
